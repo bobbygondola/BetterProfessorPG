f1e9c75af21f29c5c07c859b01d2d137fd338aec
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-11-18 20:18:33 -0500

    proc

--- knexfile.js
+++ knexfile.js
@@ -24,7 +24,7 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    migrations: { directory: '../data/migrations' },
+    migrations: { directory: './data/migrations' },
     seeds: { directory: './data/seeds' },
   },
 };
